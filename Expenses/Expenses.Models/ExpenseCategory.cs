b1a3b34b39d775537ce9530b32aff0c88492ef6f
Andrei Lucaci <contact@andreilucaci.ro>
2018-03-24 22:45:32 +0300

    Added models

---
+++ Expenses/Expenses.Models/ExpenseCategory.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace Expenses.Models
+{
+	public class ExpenseCategory
+	{
+		public int Id { get; set; }
+
+		public string Name { get; set; }
+		public IEnumerable<Expense> Expenses { get; set; }
+
+		public override bool Equals(object obj)
+			=> obj is ExpenseCategory category &&
+			   Id == category.Id;
+
+		protected bool Equals(ExpenseCategory other) => Id == other.Id;
+
+		public override int GetHashCode() => Id;
+	}
+}
---
+++ Expenses/Expenses.Repository/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using Expenses.Models;
+
+namespace Expenses.Repository
+{
+	public interface IRepository
+	{
+		ExpenseCategory GetExpenseCategory(int id);
+		IEnumerable<ExpenseCategory> GetExpenseCategories();
+		Expense GetExpense(int id);
+
+		int AddExpenseAsync(Expense expense, int categoryId);
+		int DeleteExpenseAsync(Expense expense, int categoryId);
+		int UpdateExpense(Expense expense);
+	}
+}

9c13aa77cec279259b89d099f7f516828a0d3731
Andrei Lucaci <contact@andreilucaci.ro>
2018-03-27 10:43:51 +0300

    Small ui fixes

--- Expenses/Expenses.Win/Views/ExpenseCategoryList/_ExpenseCategoryTile.Designer.cs
+++ Expenses/Expenses.Win/Views/ExpenseCategoryList/_ExpenseCategoryTile.Designer.cs
@@ -28,33 +28,35 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.titleLabel = new System.Windows.Forms.Label();
-			this.SuspendLayout();
-			// 
-			// titleLabel
-			// 
-			this.titleLabel.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.titleLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.titleLabel.Location = new System.Drawing.Point(0, 0);
-			this.titleLabel.Name = "titleLabel";
-			this.titleLabel.Size = new System.Drawing.Size(150, 150);
-			this.titleLabel.TabIndex = 0;
-			this.titleLabel.Text = "titleLabel";
-			this.titleLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-			this.titleLabel.Click += new System.EventHandler(this.titleLabel_Click);
-			// 
-			// _ExpenseCategoryTile
-			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.Controls.Add(this.titleLabel);
-			this.Name = "_ExpenseCategoryTile";
-			this.ResumeLayout(false);
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.titleLabel.Font = new System.Drawing.Font("Lucida Bright", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titleLabel.ForeColor = System.Drawing.Color.White;
+            this.titleLabel.Location = new System.Drawing.Point(0, 0);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(150, 150);
+            this.titleLabel.TabIndex = 0;
+            this.titleLabel.Text = "titleLabel";
+            this.titleLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.titleLabel.Click += new System.EventHandler(this.titleLabel_Click);
+            // 
+            // _ExpenseCategoryTile
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Controls.Add(this.titleLabel);
+            this.Name = "_ExpenseCategoryTile";
+            this.ResumeLayout(false);
 
 		}
 
-		#endregion
+        #endregion
 
-		private System.Windows.Forms.Label titleLabel;
-	}
+        private System.Windows.Forms.Label titleLabel;
+    }
 }
